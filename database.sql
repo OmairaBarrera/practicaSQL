64c001b034c4b6cad47ff2ac6ee00a06d7b9c51e
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-24 11:11:54 -0500

    feat: :sparkles: Create Firts Table
    The create firts table with of command CREATE TABLE.

--- database.sql
+++ database.sql
@@ -4,3 +4,10 @@ CREATE DATABASE campus;
 --Para poder insertar tablas y realizar algunas operaciones en la base de datos debemos
 --utilizar la base de datos con el siguiente comando USE.
 USE campus;
+--Crearemos una tabla con sus respectivos campos
+CREATE TABLE estudiantes(
+    id_estudiante INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    edad INT
+);
