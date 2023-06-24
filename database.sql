f4cd832525955660958994e22007f8829b7c90f3
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-24 12:53:41 -0500

    feat: :sparkles: Commands DML
    Exanples of the DML commands were made, that allows users to carry out the tasks of consulting or manipulating the data.

--- database.sql
+++ database.sql
@@ -33,3 +33,50 @@ ALTER TABLE notas MODIFY COLUMN calificacion FLOAT;
 --Eliminar una tabla
 DROP TABLE notas;
 
+--COMANDOS DML (DATA MANIPULATION LANGUAGE)
+--permite a los usuarios llevar a cabo las tareas de consulta o manipulación de los datos, 
+--organizados por el modelo de datos adecuado.
+
+--Insertar datos a las tablas
+--Tabla estudiantes
+INSERT INTO estudiantes (id_estudiante, nombre, apellido, edad)
+VALUES (1, "Juan", "Perez", 18),
+(2, "Maria", "Gomez", 20),
+(3, "Carlos", "Rodriguez", 19);
+
+--Tabla notas
+INSERT INTO notas (id_nota, id_estudiante, asignatura, calificacion)
+VALUES (1, 1, "Matematicas", 5.0),
+(2, 2, "Ciencias", 4.5),
+(3, 3, "Sociales", 3.0);
+
+--Actualizar datos de una tabla
+UPDATE estudiantes
+SET email = "juan@gmail.com"
+WHERE nombre = "Juan";
+
+UPDATE estudiantes
+SET email = "maria@gmail.com"
+WHERE nombre = "Maria";
+
+UPDATE estudiantes
+SET email = "carlos@gmail.com"
+WHERE nombre = "Carlos";
+
+--Eliminar una fila de la tabla
+--No podemos eliminar direntamente un dato de una tabla cuando hay otra que depende de esta
+--en este caso nos toca eliminar primero al estudiante de la tabla notas y despues de la tabla
+--estudiantes
+DELETE FROM notas WHERE id_estudiante = 2;
+DELETE FROM estudiantes WHERE id_estudiante = 2;
+
+--Consulta para obtener todos los datos de una tabla
+SELECT * FROM estudiantes;
+
+--Consultar para obtener estudiantes mayores de 18 años
+SELECT * FROM estudiantes WHERE edad > 18;
+
+--Consulta utilizando el INNER JOIN
+SELECT estudiantes.nombre, estudiantes.apellido, notas.asignatura, notas.calificacion
+FROM estudiantes
+INNER JOIN notas ON estudiantes.id_estudiante = NETWORK_NAMESPACE.id_estudiante;
