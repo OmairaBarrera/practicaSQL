198f49c3ba71c4ec4fd682933502cea8a40f93d8
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-24 11:10:41 -0500

    feat: :sparkles: Use database
    This command is to be able to use the created database.

--- database.sql
+++ database.sql
@@ -1,3 +1,6 @@
 
 --creamos una base de datos
 CREATE DATABASE campus;
+--Para poder insertar tablas y realizar algunas operaciones en la base de datos debemos
+--utilizar la base de datos con el siguiente comando USE.
+USE campus;
